7ebd1dfc0642ec943eec87a9c57f94ab46b03522
Eve Byington <emb356@bu.edu>
2020-09-02 09:47:55 -0400

    finalized plateseq pipeline to HPC

--- plateseq_post_demux.sh
+++ plateseq_post_demux.sh
@@ -9,7 +9,7 @@ pdir=$(basename $projDir)
 secondStep="/home/local/ARCS/ngs/Pipeline_ld1/Projects/PLATESEQ/${pdir}"
 
 # make project directory in PLATESEQ
-# make fastq folder based on Index#
+# make fastq folder based on Indexing#
 indexdir=$(echo $projDir | cut -d/ -f9)
 mkdir -p ${secondStep}/${indexdir}
 webdir=/home/local/ARCS/ngs/quicklinks_isilon/release
@@ -18,7 +18,7 @@ cd $secondStep
 cd $indexdir
 
 for f in $projDir/*.gz; do
-    mv $f .
+ln -s $f .
 done
 
 for samp in $(ls *.gz | cut -f 1 -d "_" | uniq); do 
@@ -30,13 +30,13 @@ for samp in $(ls *.gz | cut -f 1 -d "_" | uniq); do
     # copy barcode sample sheet
     rpi=$( echo $samp | cut -c 1-4 )
     cp /home/local/ARCS/ngs/Pipeline_ld1/References/${rpi}.csv .
-    
+ 
     # set the lane correctly
     r1=$( ls PS*_R1_* )
     lane=$( echo $r1 | cut -f 3 -d "_" | sed "s:L00::" )
     sed -i "s:8,S:${lane},S:" ${rpi}.csv
 
-    SampleSheet=$(ls *.csv)
+    SampleSheet=${secondStep}/${indexdir}/${samp}/${rpi}.csv
 
     echo "sh /home/local/ARCS/ngs/Pipeline_ld1/scripts/runDMP.sh `pwd` $SampleSheet" >> ../plate_command.list
     cd ${secondStep}/${indexdir}
@@ -44,13 +44,22 @@ done
 
 parallel --progress --joblog plate_joblog.txt < plate_command.list
 
-# Now, start working on the release
+# create a directory using ProjID name in Web/release if it doesnt already exist
 if [[ ! -e $webdir/${pdir} ]]; then
     mkdir -p $webdir/${pdir}
 fi
 
-rsync -avPh PS*_{A,G,T,C}*gz $webdir/${pdir}
-echo "running md5sum for $webdir/${pdir}"
-cd $webdir/${pdir}
+# move demultiplexed fastqs to release
+mv PS*/PS*_{A,G,T,C}*gz $webdir/${pdir}
 
-parallel "sh /home/local/ARCS/ngs/Pipeline_ld1/scripts/checkMD5_fastq.sh {} {}'fastq_md5_temp.txt' $pdir" ::: *fastq.gz   
+cd $webdir/${pdir}
+fastqs=`ls *fastq.gz | wc -l`
+
+# only intitiate md5sum creation if all 768 fastq.gzs are in release folder
+if [ "$fastqs" -eq 768 ]; then
+    echo "running md5sum for $webdir/${pdir}"
+    parallel "sh /home/local/ARCS/ngs/Pipeline_ld1/scripts/checkMD5_fastq.sh {} {}'fastq_md5_temp.txt' $pdir" ::: *fastq.gz   
+fi
+
+
+
